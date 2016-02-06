d5aaa09e12e2ba5c1480727c3c2968f1ca2c016f
Steffi <stefanie.manzinger@tum.de>
2016-02-06 21:39:09 +0100

    added min and max velocity for state straight-curve

--- src/trajectory_line_creator.cpp
+++ src/trajectory_line_creator.cpp
@@ -46,7 +46,8 @@ float TrajectoryLineCreator::targetVelocity(){
     float aOrthMax = config().get<float>("aOrthMax", 9.81*0.5);
     float curve_minVelocity = config().get<float>("curve_minVelocity", 1.8);
     float curve_maxVelocity = config().get<float>("curve_maxVelocity", 5);
-
+    float curveStraight_minVelocity = config().get<float>("curveStraight_minVelocity", 1.5);
+    float curveStraight_maxVelocity = config().get<float>("curveStraight_maxVelocity", 2.0);
 
     float curveVelocity = sqrt(aOrthMax/fabs(roadStates->states[2].curvature));
     float straightCurveVelocity = sqrt(aOrthMax/fabs(roadStates->states[1].curvature));
@@ -69,13 +70,13 @@ float TrajectoryLineCreator::targetVelocity(){
     {
         curveVelocity = curve_maxVelocity;
     }
-    if (straightCurveVelocity < curve_minVelocity)
+    if (straightCurveVelocity < curveStraight_minVelocity)
     {
-        straightCurveVelocity = curve_minVelocity;
+        straightCurveVelocity = curveStraight_minVelocity;
     }
-    if (straightCurveVelocity > curve_maxVelocity)
+    if (straightCurveVelocity > curveStraight_maxVelocity)
     {
-        straightCurveVelocity = curve_maxVelocity;
+        straightCurveVelocity = curveStraight_maxVelocity;
     }
 
     stateVelocities(1) = straightCurveVelocity;
@@ -88,11 +89,11 @@ float TrajectoryLineCreator::targetVelocity(){
 
     velocity = (stateProbabilities.cwiseProduct(stateVelocities)).sum() / stateProbabilities.sum();
 
-
     if (velocity > stateVelocities(0))
     {
         velocity = stateVelocities(0);
     }
+
     if (velocity < curve_minVelocity)
     {
         velocity = curve_minVelocity;
@@ -103,6 +104,7 @@ float TrajectoryLineCreator::targetVelocity(){
     }else{
         //do nothing
     }
+
     return velocity;
 }
 
